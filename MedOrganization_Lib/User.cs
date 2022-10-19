9d8e6e7ef1eab7d223ab6e3134e27cd294c68728
Kalymkan Aruzhan <113041589+Arururin@users.noreply.github.com>
2022-10-19 23:46:43 +0600

    Add files via upload

---
+++ MedOrganization_Lib/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MedOrganization;
+
+namespace MedOrganization.Lib
+{
+    public enum AccessRights { User, Admin }
+    public class User
+    {
+        public User()
+        {
+
+        }
+        public User(string login, string password, AccessRights accessRights, MedOrganization medOrganization)
+        {
+            this.login = login;
+            this.password = password;
+            this.accessRights = accessRights;
+            this.medOrganization = medOrganization;
+        }
+        public string login { get; set; }
+        public string password { get; set; }
+        public AccessRights accessRights { get;set; }
+        public MedOrganization medOrganization { get; set; }
+
+    }
+}
